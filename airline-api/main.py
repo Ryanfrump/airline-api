f081c1b39bf89e26a1fb5c6835e56c16e4d0c884
Ryanfrump <ryanfrump@gmail.com>
2024-05-10 09:19:29 -0600

    fixed end points

--- airline-api/main.py
+++ airline-api/main.py
@@ -43,21 +43,21 @@ for airline_name_str, flights_data in airline_dict.items():
 async def list_airlines() -> list:
     return airlines.keys()
 
-@app.get("/airlines/{airline_name}/flight_number")
+@app.get("/{airline_name}/flight_number")
 async def flight_numbers(airline_name: str) -> list:
     return [flight.flight_number for flight in airlines[airline_name].flights]
 
-@app.get("/airlines/{airline_name}/flight/{flight_number}")
+@app.get("/{airline_name}/{flight_number}/flight")
 async def print_flight(airline_name: str, flight_number: str):
     for flight in airlines[airline_name].flights:
         if flight.flight_number == flight_number:
             return flight 
         
-@app.post("/airlines/{airline_name}")
+@app.post("/{airline_name}")
 async def add_flight(airline_name: str):
     return airlines[airline_name].flights.append(flight)
 
-@app.put("/airlines/{airline_name}/flight/{flight_number}")
+@app.put("/{airline_name}/{flight_number}")
 async def update_flight(airline_name: str, flight_number: str, updated_flight: Flight):
     flights = airlines[airline_name].flights
     for i, flight in enumerate(flights):
@@ -67,7 +67,7 @@ async def update_flight(airline_name: str, flight_number: str, updated_flight: F
     flights.append(updated_flight)
     return "Flight created succesfully"
 
-@app.delete("/airlines/{airline_name}/flight/{flight_number}")
+@app.delete("/{airline_name}/{flight_number}")
 async def delete_flight(airline_name: str, flight_number: str):
     flights = airlines[airline_name].flights
     for flight in flights:
